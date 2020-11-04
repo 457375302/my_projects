13b6e34ecd14841142b4a8f0026ee69cbd7e9a14
jiang <15577363341@163.com>
2020-11-04 15:43:18 +0800

    首次

---
+++ my_projects_service/my_project_background_service/src/main/java/com/jjc/my_project_background_service/MyProjectBackgroundServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jjc.my_project_background_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjectBackgroundServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyProjectBackgroundServiceApplication.class, args);
+    }
+
+}
---
+++ my_projects_service/my_project_background_service/src/test/java/com/jjc/my_project_background_service/MyProjectBackgroundServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjc.my_project_background_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProjectBackgroundServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
